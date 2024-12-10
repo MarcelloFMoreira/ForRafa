6084ea7c47976b383c26afcc8aa0ea6409f06a7a
Marcello de Freitas Moreira <marcellofreitas1227@gmail.com>
2024-12-09 21:58:33 -0300

    ajustando carrosel

--- script.js
+++ script.js
@@ -3,13 +3,30 @@ const img = button.querySelector('img');
 
 button.addEventListener('click', function () {
     const audio = document.querySelector('audio');
-    if (audio.paused) { 
-        audio.play();   
+    if (audio.paused) {
+        audio.play();
         img.src = 'img/pause-solid 1.png'
         img.alt = 'Ouvir'
-    } else {            
-        audio.pause();  
+    } else {
+        audio.pause();
         img.src = 'img/play-solid 1.png'
         img.alt = 'Pausar'
     }
+});
+
+new Swiper('.cards', {
+    loop: true,
+
+    // bullets
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
 });
